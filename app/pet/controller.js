93b2911e88bc18b07b86a67d8b74bbe742803a1c
Manav Misra <manavm@visionwebsoft.com>
2023-02-14 19:39:38 -0600

    Add controller with create and readAll methods

--- app/pet/controller.js
+++ app/pet/controller.js
@@ -10,7 +10,13 @@ const client = new MongoClient(URI, {
 const db = client.db("ShelterDB");
 const petCollection = db.collection("pets");
 
-petCollection.insertOne({
-  name: "Fido",
-  type: "Pug",
-});
+const controller = {
+  create(newPet) {
+    return petCollection.insertOne(newPet);
+  },
+  readAll() {
+    return petCollection.find().toArray();
+  },
+};
+
+export default controller;

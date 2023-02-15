dbf095e4b461e8923bafcbb5c6db202053fcc61b
Manav Misra <manavm@visionwebsoft.com>
2023-02-14 19:51:08 -0600

    Add 18-1-1 example

--- app/index.js
+++ app/index.js
@@ -1,1 +1,25 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+import express from "express";
+import controller from "./pet/controller.js";
+
+const PORT = 3000;
+
+const app = express();
+
+// Middleware - Allows us to parse JSON
+app.use(express.json());
+
+app.post("/pet/create", (req, res) => {
+  controller.create(req.body).then((result) => {
+    res.json(result);
+  });
+});
+
+app.get("/pet/readAll", (_, res) => {
+  controller.readAll().then((result) => {
+    res.json(result);
+  });
+});
+
+app.listen(PORT, () => {
+  console.info(`Server running at http://localhost:${PORT}/`);
+});
